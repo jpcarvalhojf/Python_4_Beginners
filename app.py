936b0329e82ec45c2621de61e185fc67d4b490c9
joao.carvalho <joao.carvalho@pixeon.com>
2023-05-05 09:45:20 -0300

    :sparkles: feat: Funcionalidades básicas do Python.

---
+++ app.py
@@ -0,0 +1,83 @@
+import datetime
+
+#INPUT AND PRINT
+'''
+age = 20
+name = 'João'
+price = 100.65
+is_online = False
+'''
+'''
+user_name = input("What's your name? ")
+
+print("Hello " + user_name)
+'''
+
+#CONVERSION TYPES
+'''
+float()
+int()
+str()
+bool()
+'''
+
+#GET CURRENT DATE CALC THE AGE
+'''
+birth_year = input("Let get your age! \n Tell me your birth year: ")
+
+today = datetime.date.today()
+
+current_year = today.year
+
+age = current_year - int(birth_year)
+
+print("Your age is "+ str(age))
+'''
+
+#CONVERT INPUT TO FLOAT
+'''
+value1 = float(input("First "))
+value2 = float(input("Second "))
+
+sum = value1 + value2
+
+print("Total: " + str(sum))
+
+'''
+
+#STRING FUNCTIONS
+'''
+course = 'Python for Beginners'
+#print(course.upper())
+#print(course.lower())
+#print(course.find('y'))
+#print(course.replace('for', '4'))
+print('Pithon' in course)
+'''
+
+#ARITHMETIC OPERATIONS
+'''
+variable1 = 3
+variable2 = 2
+
+sum = variable1 + variable2
+dif = variable1 - variable2
+multi =  variable1 * variable2
+div =  variable1 / variable2
+divRound =  variable1 // variable2
+remain = variable1 % variable2
+exponentOperator = variable1 ** variable2
+
+x = 3
+x = x + 3
+x += 2
+x *= 4
+
+print('Sum: ' + str(sum))
+print('Diff: ' + str(dif))
+print('Mult: ' + str(multi))
+print('Div: ' + str(div))
+print('DivRound: ' + str(divRound))
+print('Exponential: ' + str(exponentOperator))
+print('x: ' + str(x))
+'''
